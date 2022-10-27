afb763b03b8ad408c9ad8d17ec5fe85c05c7540e
7568 <18508227568@163.com>
2022-10-27 14:19:57 +0800

    init

--- sh/train_v2_restart.sh
+++ sh/train_v2_restart.sh
@@ -5,4 +5,4 @@ rm -f pid/train_v2.pid
 
 rm -rf log/train_v2_*
 echo 'gpu_index : '${1:-0}
-python ../train_v2.py --log_to_file --gpu_index ${1:0} & echo $! >> pid/train_v2.pid
+python ../train_v2.py --log_to_file --gpu_index ${1:-0} & echo $! >> pid/train_v2.pid
